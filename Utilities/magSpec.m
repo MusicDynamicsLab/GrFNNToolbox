8321649fc56a13e4ee8ed815e58d0fe01f770cc4
Karl Lerud <karl.lerud@uconn.edu>
2015-03-10 16:34:06 -0400

    Small change to FFT code

--- Utilities/magSpec.m
+++ Utilities/magSpec.m
@@ -5,17 +5,20 @@ if ~isreal(x)
     warning('Input signal is complex; only real portion taken')
     x=real(x);
 end
-if length(varargin)==2
+if length(varargin)>1
     reference=varargin{2};
 else
     reference=1;
 end
 flag=0;
 if size(x,1)==1, x=x';flag=1;end
-% correction=1/size(x,1);
-correction=1;
-y=abs(fft(x,NFFT))+1;
-% y=abs(fft(x,NFFT))*2;
+if length(varargin)>1
+    correction=1/size(x,1);
+    y=abs(fft(x,NFFT))*2;
+else
+    correction=1;
+    y=abs(fft(x,NFFT))+1;
+end
 len=size(y,1);
 ind=floor(len/2);
 if flag
