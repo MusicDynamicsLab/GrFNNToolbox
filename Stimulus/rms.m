66d3a9393565af962354cec5b8707f93bf6934c0
Karl Lerud <karl.lerud@uconn.edu>
2015-07-29 15:01:09 -0400

    Fix mistake in rms

--- Stimulus/rms.m
+++ Stimulus/rms.m
@@ -1,11 +1,3 @@
 function RMS = rms(x)
-
-if length(x) == sum(size(x))-1 && size(x,2) > 1
-    x = x.';
-end
-
-RMS = zeros(1,size(x,2));
-
-for i = 1:length(RMS)
-    RMS(i) = sqrt(sum(real(x(:,i)).^2)/size(x,1));
-end
+% This function works along the first non-singleton dimension of x
+RMS = sqrt(mean(x .* conj(x)));
