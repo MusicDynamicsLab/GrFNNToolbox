9678eb0061e73adb21868162d6295cb39e82615d
danharpin <daniel@Billie.local>
2016-02-02 12:11:09 -0500

    Enable live stimulusDisplay when targeting GPU
    The stimulus can now be displayed live on the GPU.

--- Stimulus/stimulusDisplay.m
+++ Stimulus/stimulusDisplay.m
@@ -1,10 +1,17 @@
 %% function: Displays stimulus and progress bar
-function bH = stimulusDisplay(stim, ix, t)
-% global M 
-if ix == 0
+function [] = stimulusDisplay(stim, ix, t)
+% global M
+persistent stimulusDispMap;
+
+if ix == 0  
+    if (size(stimulusDispMap,2) < stim.id)
+        stimulusData.id = stim.id;
+        stimulusDispMap{stim.id} = stimulusData;
+    end
+    
     sx = stim.id;
     if isfield(stim,'sAx') && ishghandle(stim.sAx)
-        axes(stim.sAx)
+        stimulusData.sAx = axes(stim.sAx);
     else
         figure(10000+sx)
     end
@@ -12,21 +19,23 @@ if ix == 0
     plot(stim.t, real(stim.x(stim.dispChan,:)), 'k')
     hold on
     ylimit = get(gca,'YLim');
-    bH = plot([1 1]*t, ylimit, 'r'); % handle for progress bar
-    set(gca,'YLim',ylimit) % need to do this for lastest matlab
+    stimulusData.bH = plot([1 1]*t, ylimit, 'r'); % handle for progress bar
+    set(gca,'YLim',ylimit) % need to do this for latest matlab
     set(gca,'XLim',[min(stim.t) max(stim.t)])
     hold off
     if isfield(stim,'title') && ischar(stim.title)
-        title(stim.title)
+        stimulusData.title = title(stim.title);
     else
-        title(['Stimulus ' num2str(sx) ', Channel ', num2str(stim.dispChan)])
+        stimulusData.title = ...
+            title(['Stimulus ' num2str(sx) ', Channel ', num2str(stim.dispChan)]);
     end
     xlabel('Time')
     ylabel('Real part')
     grid
-    
+    stimulusDispMap{stim.id} = stimulusData;
 else
-    set(stim.bH, 'XData', [1 1]*t)
+    stimulusData = stimulusDispMap{stim.id};
+    set(stimulusData.bH, 'XData', [1 1]*t)
 end
 
 drawnow
