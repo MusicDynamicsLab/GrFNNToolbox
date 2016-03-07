89afe147a06507361a4a834094337d0ba9a7e869
Ji Chul Kim <jichulkim21@gmail.com>
2016-03-07 12:28:35 -0500

    Fix examples to have odd number of oscillators

--- Examples/example1P.m
+++ Examples/example1P.m
@@ -18,14 +18,15 @@ lambda = .001; mu1 = -1; mu2 = -50; ceps = 16; kappa = 1; % Supercritical learni
 s = stimulusMake(1, 'fcn', [0 100], 40, {'exp'}, 1, 0);
 
 n = networkMake(1, 'hopf', alpha, beta1,  beta2, 0, 0, neps, ...
-    'log', .5, 2, 200, 'save', 1, ...
+    'log', .5, 2, 201, 'save', 1, ...
     'display', 10, 'Tick', [.5 .67 .75 1 1.25 1.33 1.50 2]);
 
 n = connectAdd(n, n, [], 'weight', w, 'type', 'all2freq', ...
     'learn', lambda, mu1, mu2, ceps, kappa, ...
     'display', 10,'phasedisp', 'save', 500);
 
-M = modelMake(@zdot, @cdot, s, n);
+% M = modelMake(@zdot, @cdot, s, n);
+evalc('M = modelMake(@zdot, @cdot, s, n);');
         % The network is not connected to the stimulus, but the model needs
         % a stimulus to get a time vector
 
