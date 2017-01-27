d7d9d42254de2464dd6c3739932cb62cee8c787c
Hermann Helmholtz <Karl Lerud>
2017-01-27 16:03:04 -0500

    Start GPU optimization validations

--- Functions/zdotAdaptive.m
+++ Functions/zdotAdaptive.m
@@ -14,24 +14,26 @@
 %%
 function [dzdt] = zdotAdaptive(t, zColumn, M)
 
-numberNetworks = length(M.n);
-zMat = cell(numberNetworks,1);
-dzdtMat = cell(numberNetworks,1);
-ind = 1;
-zColumnLength = 0;
-for i = 1:numberNetworks
-    zMat{i} = zColumn(ind:M.n{i}.N + ind -1);
-    ind = ind + M.n{i}.N;
-    zColumnLength = zColumnLength + M.n{i}.N;
-end
+% numberNetworks = length(M.n);
+% zMat = cell(numberNetworks,1);
+% dzdtMat = cell(numberNetworks,1);
+% ind = 1;
+% zColumnLength = 0;
+% for i = 1:numberNetworks
+%     zMat{i} = zColumn(ind:M.n{i}.N + ind -1);
+%     ind = ind + M.n{i}.N;
+%     zColumnLength = zColumnLength + M.n{i}.N;
+% end
+
+nOsc = length(zColumn)/2;
 
 %% Initialize variables and stimulus
 
-for nx = 1:numberNetworks
+% for nx = 1:numberNetworks
     
-    z = zMat{nx};
+    z = zColumn(1:nOsc);
     
-    n   = M.n{nx};
+    n   = M.n{1};
     % z   = n.z;
     a   = n.a;
     b1  = n.b1;
@@ -44,28 +46,44 @@ for nx = 1:numberNetworks
     
     for cx = 1:length(n.con)
         con = n.con{cx};
-        if con.nSourceClass == 1
-            %         y = M.s{con.source}.z;
-            y = stimulusRun(M.s{1}.x,t*M.fs+1);
-        else
-%             y = M.n{con.source}.z;
-            y = zMat{con.source};
-        end
+        y = stimulusRun(M.s{1}.x,t*M.fs+1);
+        x = x + con.w.*(con.C*y);
+    end
+    
+    %% The differential equation
+    % $\dot{z} = z \left( \alpha + \textrm{i}\omega + (\beta_1 + \textrm{i}\delta_1) |z|^2 + \frac{\epsilon (\beta_2 + \textrm{i}\delta_2) |z|^4}{1-\epsilon |z|^2} \right) + x$
+    dzdt1 = z.*(a + b1.*abs(z).^2 + e*b2.*(abs(z).^4)./(1-e*abs(z).^2)) + x;
+    
+% end
+
+
+%%
+
+
+
+
+%% Initialize variables and stimulus
+
+% for nx = 1:numberNetworks
+    
+    z = zColumn(nOsc+1:end);
+    
+    n   = M.n{2};
+    % z   = n.z;
+    a   = n.a;
+    b1  = n.b1;
+    b2  = n.b2;
+    e   = n.e;
+    
+    %% Add input
+    
+    x = 0;
+    
+    for cx = 1:length(n.con)
+        con = n.con{cx};
+       
+        y = zColumn(1:nOsc);
         
-        switch con.nType    % cases ordered by frequency of use
-            
-            case 1  % 1freq
-                x = x + con.w.*(con.C*y);
-                
-            case 6  % all2freq
-                if con.no11
-                    x = x + con.w .* sum(con.C.*( A(e, z)*P(e, y.') ...
-                        - A(e^2, z*y').*repmat(y.', con.targetN, 1) ), 2);
-                else
-                    x = x + con.w .* sum(con.C.*( A(e, z)*P(e, y.') ), 2);
-                end
-                
-            case 7  % allfreq
 %                 if con.no11
 %                     x =  x + con.w .* sum(con.C.*( A(e, z)*P_new(e, y.') ...
 %                         - A(e^2, z*y').*repmat(y.'.*A(e^2, abs(y.').^2), con.targetN, 1) ), 2);
@@ -76,46 +94,28 @@ for nx = 1:numberNetworks
 %                 x = x + con.w .* (con.C*sum(y+sqrt(e)*y.*conj(y)));
 %                 x = x + con.w .* (con.C*sum(y));
 
-                
-            case 2  % 2freq
-                NUM = con.NUM;
-                DEN = con.DEN;
-                Y = repmat(sqrt(e)*y.', con.targetN, 1).^ NUM;
-                Z = repmat(sqrt(e)*conj(z) , 1, con.sourceN).^(DEN-1);
-                x = x + con.w .* sum(con.C.*Y.*Z,2)/sqrt(e);
-                
-            case 5  % active
-                if con.no11
-                    x = x + con.w .* sum(con.C.*( (sqrt(e)*conj(z).*A(e, z))*y.' ), 2);
-                else
-                    x = x + con.w .* sum(con.C.*( A(e, z)*y.' ), 2);
-                end
-                
-            otherwise % 3freq and 3freqall
-                Y1 = sqrt(e)*y(con.IDX1); Y1(con.CON1) = conj(Y1(con.CON1));
-                Y2 = sqrt(e)*y(con.IDX2); Y2(con.CON2) = conj(Y2(con.CON2));
-                Z  = sqrt(e)*conj(z(con.IDXZ));
-                NUM1 = con.NUM1;
-                NUM2 = con.NUM2;
-                DEN = con.DEN;
-                x_int = con.C.*(Y1.^NUM1).*(Y2.^NUM2).*(Z.^(DEN-1))/sqrt(e);
-                x = x + con.w .* sum(x_int,2);
-                
-        end
+             
     end
     
     %% The differential equation
     % $\dot{z} = z \left( \alpha + \textrm{i}\omega + (\beta_1 + \textrm{i}\delta_1) |z|^2 + \frac{\epsilon (\beta_2 + \textrm{i}\delta_2) |z|^4}{1-\epsilon |z|^2} \right) + x$
-    dzdtMat{nx} = z.*(a + b1.*abs(z).^2 + e*b2.*(abs(z).^4)./(1-e*abs(z).^2)) + x;
+    dzdt2 = z.*(a + b1.*abs(z).^2 + e*b2.*(abs(z).^4)./(1-e*abs(z).^2)) + x;
+    dzdt=[dzdt1;dzdt2];
     
-end
+% end
 
-dzdt = NaN(zColumnLength,1);
-ind = 1;
-for i = 1:numberNetworks
-    dzdt(ind:ind + M.n{i}.N - 1) = dzdtMat{i};
-    ind = ind + M.n{i}.N;
-end
+
+
+
+
+
+
+% dzdt = NaN(zColumnLength,1);
+% ind = 1;
+% for i = 1:numberNetworks
+%     dzdt(ind:ind + M.n{i}.N - 1) = dzdtMat{i};
+%     ind = ind + M.n{i}.N;
+% end
 
 %%
 function s = stimulusRun(s, time)
@@ -138,6 +138,8 @@ function s = stimulusRun(s, time)
 
 leftInd = floor(time);
 here    = s(:,leftInd);
+% disp(time)
+% disp(ceil(time))
 there   = s(:,ceil(time));
 s = here + ((time - leftInd) * (there-here));
 
